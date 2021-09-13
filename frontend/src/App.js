3290f77e5e5fa1c26a09e2374002a8e85363840c
grantc00 <grantcky@gmail.com>
2021-09-13 14:40:12 -0700

    testing git

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
-
+//testing
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
