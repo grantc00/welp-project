6aa0c676d8c6ac1f3cbe29bc966b375f5eb6c12f
grantc00 <grantcky@gmail.com>
2021-09-13 21:57:31 -0700

    seeders 1 done

---
+++ backend/db/seeders/20210914035434-business.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Businesses', [
+      {ownerId: 1, title:'In-N-Out Burger', description:'In-N-Out Burger is an American regional chain of fast food restaurants with locations primarily in California and the Southwest. It was founded in Baldwin Park, California, in 1948 by Harry Snyder and Esther Snyder.', address:'445 Industrial Rd, San Carlos, CA 94070', createdAt: new Date(), updatedAt: new Date()},
+      {ownerId: 1, title:'Mcdonald', description:"McDonald's is an American fast food company, founded in 1940 as a restaurant operated by Richard and Maurice McDonald, in San Bernardino, California, United States", address:'3128 El Camino Real, Palo Alto, CA 94306', createdAt: new Date(), updatedAt: new Date()},
+      {ownerId: 1, title:"Jersey Joe's Cheesesteak", description:'Specialized in Cheesesteak made with half pound Ribeye daily sliced on premise and fresh bread baked daily in local bakery', address:'21 El Camino Real, San Carlos, CA 94070', createdAt: new Date(), updatedAt: new Date()},
+      {ownerId: 1, title:"Wendy's", description:"Wendy's is an American international fast food restaurant chain founded by Dave Thomas on November 15, 1969, in Columbus, Ohio.", address:'698 Ralston Avenue, Belmont, CA 94002', createdAt: new Date(), updatedAt: new Date()},
+      {ownerId: 1, title:'Chick-fil-A', description:"Chick-fil-A is one of the largest American fast food restaurant chains and the largest whose specialty is chicken sandwiches.", address:'536 Whipple Ave, Redwood City, CA 94063', createdAt: new Date(), updatedAt: new Date()},
+      {ownerId: 1, title:'Dog Haus', description:"Dog Haus creates hand-crafted hormone- and antibiotic-free hot dogs, sausages, burgers, plant-based proteins and one Bad Mutha Clucka, all served on King's Hawaiian rolls. Crush one, then wash it down with one of our local craft beers or premium shakes.", address:'1000 El Camino Real, Belmont, CA 94002', createdAt: new Date(), updatedAt: new Date()},
+      {ownerId: 1, title:'Jack in the Box', description:'Jack in the Box is an American fast-food restaurant chain founded February 21, 1951, by Robert O.', address:'1100 El Camino Real, San Carlos, CA 94070', createdAt: new Date(), updatedAt: new Date()},
+      {ownerId: 1, title:'Taco Bell', description:'Taco Bell is an American-based chain of fast food restaurants originating in Irvine, California in 1962, by founder Glen Bell. Taco Bell is a subsidiary of Yum!', address:'259 El Camino Real, San Carlos, CA 94070', createdAt: new Date(), updatedAt: new Date()},
+      {ownerId: 1, title:'Shake Shack', description:'Shake Shack is an American fast casual restaurant chain based in New York City.', address:'33 Hillsdale Shopping Ctr, San Mateo, CA 94403', createdAt: new Date(), updatedAt: new Date()},
+      {ownerId: 1, title:'Panda Express', description:'Panda Express is an American fast food restaurant chain that serves American Chinese cuisine. ', address:'1050 El Camino Real, Belmont, CA 94002', createdAt: new Date(), updatedAt: new Date()},
+    ]);
+    /*
+    title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Businesses', null, {});
+
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
---
+++ backend/db/seeders/20210914035449-review.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Reviews', [
+      {userId: 2, businessId: 1, rating: 3, answer: "My favorite spot", createdAt: new Date(), updatedAt: new Date()},
+      {userId: 2, businessId: 2, rating: 5, answer: "One of the best take out, fast food experiences ever.", createdAt: new Date(), updatedAt: new Date()},
+      {userId: 2, businessId: 3, rating: 5, answer: "This place is awesome, I am doing my first online order here. It's so easy and fast.", createdAt: new Date(), updatedAt: new Date()},
+      {userId: 2, businessId: 4, rating: 3, answer: "The food was horrible", createdAt: new Date(), updatedAt: new Date()},
+      {userId: 3, businessId: 5, rating: 4, answer: "The food is good, hot and fresh", createdAt: new Date(), updatedAt: new Date()},
+      {userId: 3, businessId: 6, rating: 2, answer: "Service is horrible the language barrier is obvious.", createdAt: new Date(), updatedAt: new Date()},
+      {userId: 3, businessId: 7, rating: 1, answer: "Ok the chicken sandwich is a sleeper hit!", createdAt: new Date(), updatedAt: new Date()},
+      {userId: 3, businessId: 8, rating: 3, answer: "Large variety of interesting hot dogs", createdAt: new Date(), updatedAt: new Date()},
+    ]);
+    /*
+    userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Users'}
+      },
+      businessId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Businesses'}
+      },
+      rating: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      answer: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Reviews', null, {});
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
